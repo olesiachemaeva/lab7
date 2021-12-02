ee3c107b841755714b83920ed4edfdfc93cd25db
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2021-12-02 22:20:23 +1000

    Добавьте файлы проекта.

---
+++ lab7.2.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинну ребра куба:");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Обьем куба:" + Math.Pow(a, 3) + " куб.см");
+            Console.WriteLine("Площадь поверхности:" + Math.Pow(a, 2) * 6 + " кв.см");//площать стороны * 6 сторон куба
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    class Program
+    {
+        public struct Triangle
+        {
+            public double a, b, c;
+
+        }
+        static void Main(string[] args)
+        {
+            {
+                int a1 = 6; int b1 = 6; int c1 = 7;
+                int a2 = 6; int b2 = 14; int c2 = 15;
+                Console.WriteLine("S1 = {0}, S2 = {1}", Geron(a1, b1, c1), Geron(a2, b2, c2));
+                Console.Read();
+            }
+             double Geron(int a, int b, int c)
+            {
+                double p = (a + b + c) / 2;
+                double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                return S;
+            }
+            Console.ReadLine();
+        }
+        }
+}
